dbc090d08417890b210515220a5bbf63fb1bae70
Eric Wright <dev@ericwright.me>
2013-03-08 18:54:43 -0500

    Clean up layout. Set Google fonts back to source so the data is cached. Scroll elements into view.

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,4 +1,3 @@
-var sleepNow;
 
 window.addEventListener("load", function() {
   return setTimeout(function() {
@@ -6,44 +5,51 @@ window.addEventListener("load", function() {
   }, 0);
 });
 
-$("#sleep-now").click(function(e) {
-  $(this).parent('.btn-container').slideUp();
-  e.preventDefault();
-  return sleepNow();
-});
-
-$('#calculate-wake-time').click(function(e) {
-  e.preventDefault();
-  return $('#wake-up-time').focus();
-});
-
-$('#wake-up-time').change(function(e) {
-  var $input;
-  $input = $(e.target);
-  return $('.wake-up-container').toggleClass('set');
-});
-
-sleepNow = function() {
-  var $wakeTimes, i, minutes, now, sleepPrep, wakeTime, wakeTimes, _i, _len;
-  $wakeTimes = $('#wake-times');
-  sleepPrep = 14;
-  now = new Date();
-  wakeTimes = [];
-  i = 0;
-  while (i < 6) {
-    minutes = now.getMinutes();
-    if (i === 0) {
-      now.setMinutes(minutes + sleepPrep + 90);
-    } else {
-      now.setMinutes(minutes + 90);
+$(function() {
+  var scrollTo, sleepNow;
+  $("#sleep-now").click(function(e) {
+    $(this).parent('.btn-container').slideUp();
+    e.preventDefault();
+    return sleepNow();
+  });
+  $('#calculate-wake-time').click(function(e) {
+    e.preventDefault();
+    return $('#wake-up-time').focus();
+  });
+  $('#wake-up-time').change(function(e) {
+    var $input;
+    $input = $(e.target);
+    return $('.wake-up-container').toggleClass('set');
+  });
+  sleepNow = function() {
+    var $wakeTimes, i, minutes, now, sleepPrep, wakeTime, wakeTimes, _i, _len;
+    $wakeTimes = $('#wake-times');
+    sleepPrep = 14;
+    now = new Date();
+    wakeTimes = [];
+    i = 0;
+    while (i < 6) {
+      minutes = now.getMinutes();
+      if (i === 0) {
+        now.setMinutes(minutes + sleepPrep + 90);
+      } else {
+        now.setMinutes(minutes + 90);
+      }
+      wakeTimes.push(now.toTimeString().substr(0, 5));
+      i++;
     }
-    wakeTimes.push(now.toTimeString().substr(0, 5));
-    i++;
-  }
-  $wakeTimes.html('');
-  for (i = _i = 0, _len = wakeTimes.length; _i < _len; i = ++_i) {
-    wakeTime = wakeTimes[i];
-    $wakeTimes.append("<div>        <input class='wake-time' type='time' value='" + wakeTime + "' disabled data-wellness='" + i + "'/>       </div>");
-  }
-  return $(".wake-time-explanation, .bed-time").fadeIn();
-};
+    $wakeTimes.html('');
+    for (i = _i = 0, _len = wakeTimes.length; _i < _len; i = ++_i) {
+      wakeTime = wakeTimes[i];
+      $wakeTimes.append("<div>          <input class='wake-time' type='time' value='" + wakeTime + "' disabled data-wellness='" + i + "'/>         </div>");
+    }
+    return $(".wake-time-explanation, .wake-up-at, .share").fadeIn(function() {
+      return scrollTo('.wake-up-at');
+    });
+  };
+  return scrollTo = function(element) {
+    return $('body').animate({
+      scrollTop: $(element).offset().top
+    }, 200);
+  };
+});
--- src/javascripts/application.coffee
+++ src/javascripts/application.coffee
@@ -2,45 +2,48 @@ window.addEventListener "load", ->
   setTimeout ->
     window.scrollTo(0, 1)
   , 0
+$ ->
+  $("#sleep-now").click (e) ->
+    $(@).parent('.btn-container').slideUp()
+    e.preventDefault()
+    sleepNow()
 
-$("#sleep-now").click (e) ->
-  $(@).parent('.btn-container').slideUp()
-  e.preventDefault()
-  sleepNow()
+  $('#calculate-wake-time').click (e) ->
+    e.preventDefault()
+    $('#wake-up-time').focus()
 
-$('#calculate-wake-time').click (e) ->
-  e.preventDefault()
-  $('#wake-up-time').focus()
+  $('#wake-up-time').change (e) ->
+    $input = $(e.target)
+    $('.wake-up-container').toggleClass('set')
 
-$('#wake-up-time').change (e) ->
-  $input = $(e.target)
-  $('.wake-up-container').toggleClass('set')
+  sleepNow = ->
+    $wakeTimes = $('#wake-times')
+    # 14 minutes to sleep.
+    sleepPrep = 14
 
-sleepNow = ->
-  $wakeTimes = $('#wake-times')
-  # 14 minutes to sleep.
-  sleepPrep = 14
+    now = new Date()
+    wakeTimes = []
 
-  now = new Date()
-  wakeTimes = []
+    i = 0
+    while i < 6
+      minutes = now.getMinutes()
+      if i == 0
+        now.setMinutes(minutes + sleepPrep + 90)
+      else
+        now.setMinutes(minutes + 90)
 
-  i = 0
-  while i < 6
-    minutes = now.getMinutes()
-    if i == 0
-      now.setMinutes(minutes + sleepPrep + 90)
-    else
-      now.setMinutes(minutes + 90)
+      wakeTimes.push(now.toTimeString().substr(0,5))
+      i++
 
-    wakeTimes.push(now.toTimeString().substr(0,5))
-    i++
+    $wakeTimes.html('')
+    for wakeTime, i in wakeTimes
+      $wakeTimes.append(
+        "<div>
+          <input class='wake-time' type='time' value='#{wakeTime}' disabled data-wellness='#{i}'/>
+         </div>")
 
-  $wakeTimes.html('')
-  for wakeTime, i in wakeTimes
-    $wakeTimes.append(
-      "<div>
-        <input class='wake-time' type='time' value='#{wakeTime}' disabled data-wellness='#{i}'/>
-       </div>")
+    $(".wake-time-explanation, .wake-up-at, .share").fadeIn ->
+      scrollTo('.wake-up-at')
 
-  $(".wake-time-explanation, .bed-time").fadeIn()
-  # console.log wakeTimes
+  scrollTo = (element) ->
+    $('body').animate { scrollTop: $(element).offset().top }, 200
